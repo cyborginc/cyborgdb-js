02a4a40d1167341469eb8c6f292b06005da212f4
“Truman <trumanngny@yahoo.com>
2026-05-20 10:50:43 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,50 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './runtime';
-export * from './apis/index';
-export * from './models/index';
+// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
+export const VERSION = require('../package.json').version;
+
+// Main SDK exports
+export { CyborgDB as Client } from './client';
+export { EncryptedIndex } from './encryptedIndex';
+
+// Export commonly used types and interfaces
+export {
+  CreateIndexRequest,
+  IndexOperationRequest,
+  UpsertRequest,
+  BatchQueryRequest,
+  TrainRequest,
+  DeleteRequest,
+  GetRequest,
+  VectorItem,
+  GetResponseModel,
+  QueryResponse,
+  ErrorResponseModel,
+  HTTPValidationError,
+  QueryResultItem
+} from './models';
+
+// Export custom strongly-typed interfaces
+export {
+  JsonValue,
+  JsonObject,
+  JsonArray,
+  JsonPrimitive,
+  VectorMetadata,
+  FilterExpression,
+  FilterValue,
+  FilterOperator,
+  UpsertResponse,
+  DeleteResponse,
+  TrainResponse,
+  HealthResponse,
+  TrainingStatus,
+  GetResultItem,
+  isJsonValue,
+  isError,
+  getErrorMessage
+} from './types';
+
+// Export integrations
+export * from './integrations';
+
+// Export demo utilities
+export { getDemoApiKey } from './demo';
